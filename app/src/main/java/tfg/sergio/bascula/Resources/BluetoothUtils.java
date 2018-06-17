ad7dc0ad3613e8e3a104fce85b6ab7d1ad2036a7
sbh896 <sergiobahe@usal.es>
2018-06-17 19:46:02 +0200

    Error

---
+++ app/src/main/java/tfg/sergio/bascula/Resources/BluetoothUtils.java
@@ -0,0 +1,125 @@
+package tfg.sergio.bascula.Resources;
+
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+import android.support.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by yeyo on 16/06/2018.
+ */
+
+public class BluetoothUtils {
+    // Characteristics
+    private static final String SERVICE_STRING = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
+    private static final String  CHARACTERISTIC_ECHO_STRING = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
+    public static List<BluetoothGattCharacteristic> findCharacteristics(BluetoothGatt bluetoothGatt) {
+        List<BluetoothGattCharacteristic> matchingCharacteristics = new ArrayList<>();
+
+        List<BluetoothGattService> serviceList = bluetoothGatt.getServices();
+        BluetoothGattService service = BluetoothUtils.findService(serviceList);
+        if (service == null) {
+            return matchingCharacteristics;
+        }
+
+        List<BluetoothGattCharacteristic> characteristicList = service.getCharacteristics();
+        for (BluetoothGattCharacteristic characteristic : characteristicList) {
+            if (isMatchingCharacteristic(characteristic)) {
+                matchingCharacteristics.add(characteristic);
+            }
+        }
+
+        return matchingCharacteristics;
+    }
+
+    @Nullable
+    public static BluetoothGattCharacteristic findEchoCharacteristic(BluetoothGatt bluetoothGatt) {
+        return findCharacteristic(bluetoothGatt, CHARACTERISTIC_ECHO_STRING);
+    }
+
+    @Nullable
+    private static BluetoothGattCharacteristic findCharacteristic(BluetoothGatt bluetoothGatt, String uuidString) {
+        List<BluetoothGattService> serviceList = bluetoothGatt.getServices();
+        BluetoothGattService service = BluetoothUtils.findService(serviceList);
+        if (service == null) {
+            return null;
+        }
+
+        List<BluetoothGattCharacteristic> characteristicList = service.getCharacteristics();
+        for (BluetoothGattCharacteristic characteristic : characteristicList) {
+            if (characteristicMatches(characteristic, uuidString)) {
+                return characteristic;
+            }
+        }
+
+        return null;
+    }
+
+    public static boolean isEchoCharacteristic(BluetoothGattCharacteristic characteristic) {
+        return characteristicMatches(characteristic, CHARACTERISTIC_ECHO_STRING);
+    }
+
+    private static boolean characteristicMatches(BluetoothGattCharacteristic characteristic, String uuidString) {
+        if (characteristic == null) {
+            return false;
+        }
+        UUID uuid = characteristic.getUuid();
+        return uuidMatches(uuid.toString(), uuidString);
+    }
+
+    private static boolean isMatchingCharacteristic(BluetoothGattCharacteristic characteristic) {
+        if (characteristic == null) {
+            return false;
+        }
+        UUID uuid = characteristic.getUuid();
+        return matchesCharacteristicUuidString(uuid.toString());
+    }
+
+    private static boolean matchesCharacteristicUuidString(String characteristicIdString) {
+        return uuidMatches(characteristicIdString, CHARACTERISTIC_ECHO_STRING);
+    }
+
+    public static boolean requiresResponse(BluetoothGattCharacteristic characteristic) {
+        return (characteristic.getProperties() & BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE) != BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE;
+    }
+
+    public static boolean requiresConfirmation(BluetoothGattCharacteristic characteristic) {
+        return (characteristic.getProperties() & BluetoothGattCharacteristic.PROPERTY_INDICATE) == BluetoothGattCharacteristic.PROPERTY_INDICATE;
+    }
+
+    // Service
+
+    private static boolean matchesServiceUuidString(String serviceIdString) {
+        return serviceIdString.equals(SERVICE_STRING);
+    }
+
+    @Nullable
+    private static BluetoothGattService findService(List<BluetoothGattService> serviceList) {
+        for (BluetoothGattService service : serviceList) {
+            String serviceIdString = service.getUuid()
+                    .toString();
+            if (matchesServiceUuidString(serviceIdString)) {
+                return service;
+            }
+        }
+        return null;
+    }
+
+    // String matching
+
+    // If manually filtering, substring to match:
+    // 0000XXXX-0000-0000-0000-000000000000
+    private static boolean uuidMatches(String uuidString, String... matches) {
+        for (String match : matches) {
+            if (uuidString.equalsIgnoreCase(match)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
--- app/src/main/java/tfg/sergio/bascula/bascula/basculaFragment.java
+++ app/src/main/java/tfg/sergio/bascula/bascula/basculaFragment.java
@@ -1,15 +1,33 @@
 package tfg.sergio.bascula.bascula;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.speech.tts.TextToSpeech;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,15 +47,21 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.io.UnsupportedEncodingException;
 import java.security.PrivateKey;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Random;
+import java.util.UUID;
 
+import tfg.sergio.bascula.Manifest;
 import tfg.sergio.bascula.Models.Centro;
 import tfg.sergio.bascula.Models.Paciente;
 import tfg.sergio.bascula.Models.PacientesMesCentro;
@@ -45,6 +69,7 @@ import tfg.sergio.bascula.Models.RegistroPaciente;
 import tfg.sergio.bascula.Pacientes.AniadirPacienteFragment;
 import tfg.sergio.bascula.R;
 import tfg.sergio.bascula.Registro;
+import tfg.sergio.bascula.Resources.BluetoothUtils;
 import tfg.sergio.bascula.Resources.EnumIMC;
 import tfg.sergio.bascula.Resources.EnumIMCFirebase;
 import tfg.sergio.bascula.Resources.IMCCalculator;
@@ -60,10 +85,29 @@ public class basculaFragment extends Fragment implements TextToSpeech.OnInitList
     private String key = "";
     private int estado;
     private String centro, nombre;
+    private Handler mHandler;
     private Paciente paciente;
     private TextToSpeech mTts;
     private Handler viewHandler;
     private Bundle bundle;
+    private BluetoothAdapter mBluettothAdapter;
+    private BluetoothLeScanner mBluetoothLeScanner;
+    private boolean mEchoInitialized;
+
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final int REQUEST_FINE_LOCATION = 2;
+    private static final int SCAN_PERIOD = 2000;
+    private static final String SERVICE_UUID = "6e400001-b513-f393-e0a9-e50e24dcca9e";
+    private static final UUID CHARACTERISTIC_UUID = UUID.fromString("6e400002-b5a3-f393-e0a9-e50e24dcca9e");
+    private boolean mScanning;
+    private Map<String, BluetoothDevice> mScanResults;
+    private ScanCallback mScanCallback;
+    private boolean mConnected;
+    private BluetoothGatt mGatt;
+
+
+
 
     ProgressBar progressBar;
 
@@ -79,6 +123,12 @@ public class basculaFragment extends Fragment implements TextToSpeech.OnInitList
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        if(!getActivity().getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)){
+            return;
+        }
+
+        BluetoothManager bluetoothManager = (BluetoothManager) getActivity().getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluettothAdapter = bluetoothManager.getAdapter();
         mDatabase = FirebaseDatabase.getInstance().getReference("registros");
         mDatabase2 = FirebaseDatabase.getInstance().getReference("pacientes");
         mDatabaseCentros = FirebaseDatabase.getInstance().getReference("centros");
@@ -100,36 +150,292 @@ public class basculaFragment extends Fragment implements TextToSpeech.OnInitList
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Random rand = new Random();
-                Double n = rand.nextDouble() * (20) ;
-                Double m = rand.nextDouble() * (1.90);
+//                Random rand = new Random();
+//                Double n = rand.nextDouble() * (20) ;
+//                Double m = rand.nextDouble() * (1.90);
+//
+//                final RegistroPaciente regis = new RegistroPaciente(key,n,m, Calendar.getInstance().getTime());
+//
+//                progressBar.setVisibility(View.INVISIBLE);
+//                FragmentManager fm = getFragmentManager();
+//                FragmentTransaction ft = fm.beginTransaction();
+//                ft.addToBackStack("bascula");
+//                bundle.putDouble("peso",n);
+//                bundle.putDouble("altura",m);
+//                basculaResultFragment fragment = new basculaResultFragment();
+//                fragment.setArguments(bundle);
+//                ft.replace(R.id.pacientes_screen,fragment);
+//                ft.commit();
+//                Toast.makeText(getActivity(), "new one", Toast.LENGTH_SHORT).show();
 
-                final RegistroPaciente regis = new RegistroPaciente(key,n,m, Calendar.getInstance().getTime());
+                BluetoothGattCharacteristic characteristic = BluetoothUtils.findEchoCharacteristic(mGatt);
+                if (characteristic == null) {
+                    disconnectGattServer();
+                    return;
+                }
 
-                progressBar.setVisibility(View.INVISIBLE);
-                FragmentManager fm = getFragmentManager();
-                FragmentTransaction ft = fm.beginTransaction();
-                ft.addToBackStack("bascula");
-                bundle.putDouble("peso",n);
-                bundle.putDouble("altura",m);
-                basculaResultFragment fragment = new basculaResultFragment();
-                fragment.setArguments(bundle);
-                ft.replace(R.id.pacientes_screen,fragment);
-                ft.commit();
-                Toast.makeText(getActivity(), "new one", Toast.LENGTH_SHORT).show();
+                String message = "P";
 
+
+                byte[] messageBytes = new byte[0];
+                try {
+                    messageBytes = message.getBytes("UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                if (messageBytes.length == 0) {
+                    return;
+                }
+
+                characteristic.setValue(messageBytes);
+                boolean success = mGatt.writeCharacteristic(characteristic);
+                if (success) {
+                    Toast.makeText(getActivity(), "P enviada", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getActivity(), "Error en envío", Toast.LENGTH_SHORT).show();
+                }
 
             }
         });
+        escanear();
         super.onViewCreated(view, savedInstanceState);
     }
+
+    //region bluetooth
+    private void escanear(){
+        if(!comprobarPermisos() ){//|| mScanning){
+            return;
+        }
+        List<ScanFilter> filters = new ArrayList<>();
+        ScanSettings settings = new ScanSettings.Builder()
+                .setScanMode(ScanSettings.SCAN_MODE_LOW_POWER)
+                .build();
+        mScanResults = new HashMap<>();
+        mScanCallback = new BtleScanCallback(mScanResults);
+        mBluetoothLeScanner = mBluettothAdapter.getBluetoothLeScanner();
+        mBluetoothLeScanner.startScan(filters,settings,mScanCallback);
+        mScanning = true;
+        mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (mScanning && mBluettothAdapter != null && mBluettothAdapter.isEnabled() && mBluetoothLeScanner != null) {
+                    mBluetoothLeScanner.stopScan(mScanCallback);
+                    scanComplete();
+                }
+
+                mScanCallback = null;
+                mScanning = false;
+                mHandler = null;
+            }
+        }, 2000);
+
+    }
+
+    private class BtleScanCallback extends ScanCallback {
+        private Map<String, BluetoothDevice> mScanResults;
+
+        BtleScanCallback(Map<String, BluetoothDevice> scanResults) {
+            mScanResults = scanResults;
+        }
+
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            addScanResult(result);
+        }
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            for (ScanResult result : results) {
+                addScanResult(result);
+            }
+        }
+        @Override
+        public void onScanFailed(int errorCode) {
+        }
+        private void addScanResult(ScanResult result) {
+            BluetoothDevice device = result.getDevice();
+            String deviceAddress = device.getAddress();
+            mScanResults.put(deviceAddress, device);
+        }
+    };
+
+    private boolean comprobarPermisos(){
+        if(mBluettothAdapter == null || !mBluettothAdapter.isEnabled()) {
+            habilitarBluetooth();
+            return false;
+        }else if(!tienePermisoUbicacion()){
+            habilitarUbicacion();
+            return false;
+        }
+        return true;
+    }
+
+    private void habilitarBluetooth(){
+        Intent habilitarBt = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(habilitarBt, REQUEST_ENABLE_BT);
+    }
+
+    private boolean tienePermisoUbicacion(){
+        return ActivityCompat.checkSelfPermission(getActivity(),android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void habilitarUbicacion(){
+        requestPermissions(new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_FINE_LOCATION);
+    }
+
+    private void scanComplete() {
+        if (mScanResults.isEmpty()) {
+            return;
+        }
+        for (String deviceAddress : mScanResults.keySet()) {
+            String prueba = deviceAddress;
+            if(deviceAddress.equals("30:AE:A4:06:55:16")){
+                BluetoothDevice device = mScanResults.get(deviceAddress);
+                connectDevice(device);
+            }
+
+        }
+    }
+
+    private class GattClientCallback extends BluetoothGattCallback {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            super.onConnectionStateChange(gatt, status, newState);
+
+            if (status == BluetoothGatt.GATT_FAILURE) {
+                disconnectGattServer();
+                return;
+            } else if (status != BluetoothGatt.GATT_SUCCESS) {
+                // handle anything not SUCCESS as failure
+                disconnectGattServer();
+                return;
+            }
+
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                setConnected(true);
+                gatt.discoverServices();
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                disconnectGattServer();
+            }
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            super.onServicesDiscovered(gatt, status);
+            if (status != BluetoothGatt.GATT_SUCCESS) {
+                return;
+            }
+            List<BluetoothGattCharacteristic> matchingCharacteristics = BluetoothUtils.findCharacteristics(gatt);
+
+            for (BluetoothGattService gattService : gatt.getServices()) {
+                for (BluetoothGattCharacteristic mCharacteristic : gattService.getCharacteristics()) {
+                    mCharacteristic.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT);
+                    enableCharacteristicNotification(gatt, mCharacteristic);
+                }
+            }
+//            for (BluetoothGattCharacteristic characteristic : matchingCharacteristics) {
+//                characteristic.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT);
+//                enableCharacteristicNotification(gatt, characteristic);
+//            }
+        }
+
+        @Override
+        public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            super.onCharacteristicWrite(gatt, characteristic, status);
+            String uui = "";
+            boolean result = mGatt.readCharacteristic(characteristic);
+            UUID uuid = characteristic.getUuid();
+        }
+
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            super.onCharacteristicRead(gatt, characteristic, status);
+            String uui = "";
+            UUID uuid = characteristic.getUuid();
+        }
+
+        @Override
+        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+            super.onCharacteristicChanged(gatt, characteristic);
+            byte[] messageBytes = characteristic.getValue();
+            String message = null;
+            try {
+                message = new String(messageBytes, "UTF-8");
+                int i = 1;
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            if (message == null) {
+                return;
+            }
+
+        }
+    }
+    // Gatt connection
+
+    private void connectDevice(BluetoothDevice device) {
+        GattClientCallback gattClientCallback = new GattClientCallback();
+        mGatt = device.connectGatt(getActivity(), false, gattClientCallback);
+    }
+
+    public void setConnected(boolean connected) {
+        mConnected = connected;
+    }
+
+    public void disconnectGattServer() {
+
+        mConnected = false;
+        mEchoInitialized = false;
+
+        if (mGatt != null) {
+            mGatt.disconnect();
+            mGatt.close();
+        }
+    }
+
+    private void enableCharacteristicNotification(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+        if(!characteristic.getUuid().equals(CHARACTERISTIC_UUID)){
+                   List<BluetoothGattDescriptor> descs= characteristic.getDescriptors();
+            BluetoothGattDescriptor desc = characteristic.getDescriptor(UUID.fromString("00002902-0000-1000-8000-00805f9b34fb"));
+            if(desc != null){
+                boolean characteristicWriteSuccess = gatt.setCharacteristicNotification(characteristic, true);
+                desc.setValue(BluetoothGattDescriptor.ENABLE_INDICATION_VALUE);
+                boolean status =  gatt.writeDescriptor(desc);
+            }
+            return;
+        }
+        //habilitar notificaciones locales
+        boolean characteristicWriteSuccess = gatt.setCharacteristicNotification(characteristic, true);
+        //remotas
+//        BluetoothGattDescriptor desc = characteristic.getDescriptor(UUID.fromString("00002902-0000-1000-8000-00805f9b34fb"));
+//        BluetoothGattDescriptor desc2 = characteristic.getDescriptor(CHARACTERISTIC_UUID);
+//
+//        List<BluetoothGattDescriptor> descs= characteristic.getDescriptors();
+//        desc.setValue(BluetoothGattDescriptor.ENABLE_INDICATION_VALUE);
+//        boolean status =  gatt.writeDescriptor(desc);
+        if (characteristicWriteSuccess) {
+            if (BluetoothUtils.isEchoCharacteristic(characteristic)) {
+                initializeEcho();
+            }
+        } else {
+        }
+    }
+    public void initializeEcho() {
+        mEchoInitialized = true;
+    }
+
+    //endregion
+
+
+
+
 
     public void onActivityResult(
             int requestCode, int resultCode, Intent data) {
         if (requestCode == MY_DATA_CHECK_CODE) {
             if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
                 // success, create the TTS instance
-                mTts = new TextToSpeech(getActivity(), this);
+                //mTts = new TextToSpeech(getActivity(), this);
             } else {
                 // missing data, install it
                 Intent installIntent = new Intent();
